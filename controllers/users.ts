039a2797922077e15672850b9fa4aee277ec2823
egymatador2024 <egymatador2024@gmail.com>
2025-06-28 03:22:18 +0300

    update

--- controllers/users.ts
+++ controllers/users.ts
@@ -3,7 +3,7 @@ import { Request, Response } from 'express';
 const  getUserById=async(req:Request,res:Response)=>{
     try {
         const userid=req.user.id
-        const user=await User.findById(userid).populate("orders");
+        const user=await User.findById(userid);
         res.json({ success: true, user });
       } catch (error) {
         res.status(500).json({ success: false, message: "Fetching user failed", error });
